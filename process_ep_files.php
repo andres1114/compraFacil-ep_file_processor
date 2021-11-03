d65f1303c52009122414d1f87c79baffbaa83433
Andres Zapata <polako_1114@hotmail.com>
2021-11-02 23:16:04 -0500

    Commits first version of the ep_files processing script

--- process_ep_files.php
+++ process_ep_files.php
@@ -1,3 +1,139 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 'On');
+date_default_timezone_set('America/New_York');
+
+//Include the db connection file
+include_once "db_connection.php";
+//Include the functions file
+include_once "functions.php";
+
+$verbose_output_mode = 0;
+
+verbose(array("outputMode" => $verbose_output_mode, "outputMessage" => "Starting the ep_file process script", "logName" => "process_ep_files_php"));
+
+verbose(array("outputMode" => $verbose_output_mode, "outputMessage" => "Connecting to the CompraFacil database...", "logName" => "process_ep_files_php"));
+//Create the PDO connection objects
+$pdo_sqlite_db = pdoCreateConnection(array('db_type' => "sqlite", 'db_host' => realpath(__DIR__).'/misc_database.sqlite3', 'db_user' => "root", 'db_pass' => "", 'db_name' => ""));
+$pdo_mysql = pdoCreateConnection(array('db_type' => "mysql", 'db_host' => "192.168.10.19", 'db_user' => "root", 'db_pass' => "admin", 'db_name' => "compraFacil"));
+verbose(array("outputMode" => $verbose_output_mode, "outputMessage" => "Done", "logName" => "process_ep_files_php"));
+
+//Define the constants
+$current_script_path = realpath(__DIR__)."/";
+$epfile_folder = "ep_files";
+$ep_file_path = "/mnt/nfs/";
+$ignored_files = array('.', '..');
+
+//Get files to process
+verbose(array("outputMode" => $verbose_output_mode, "outputMessage" => "Getting the ep_files to process...", "logName" => "process_ep_files_php"));
+$query_args = array();
+$query = "SELECT * FROM epfiles_queue WHERE is_in_process = 0";
+$queryData = pdoExecuteQuery($pdo_sqlite_db, $query, $query_args, "query_1");
+verbose(array("outputMode" => $verbose_output_mode, "outputMessage" => "Done, found ".$queryData[1]." files to process", "logName" => "process_ep_files_php"));
+
+$id_array = cast_assoc_array_to_array("id", $queryData[0]);
+
+$query_args = array();
+$query = "UPDATE epfiles_queue SET is_in_process = 1 WHERE id IN (".implode(",",$id_array).")";
+pdoExecuteQuery($pdo_sqlite_db, $query, $query_args, "query_2");
+
+for ($x = 0; $x < $queryData[1]; $x++) {
+    $filename = $queryData[0][$x]["ep_file_name"];
+    $folderName = $queryData[0][$x]["date_folder"];
+
+    verbose(array("outputMode" => $verbose_output_mode, "outputMessage" => "Processing file '".$filename."'", "logName" => "process_ep_files_php"));
+    $file_content = file_get_contents($ep_file_path.$epfile_folder."/".$folderName."/".$filename);
+
+    $str_pos_domain_id_start = strpos($file_content, "DomainId:");
+    $str_pos_domain_id_end = strpos($file_content, ":DomainId");
+    $domain_id = substr($file_content, $str_pos_domain_id_start, $str_pos_domain_id_end);
+
+    verbose(array("outputMode" => $verbose_output_mode, "outputMessage" => "Domain id: $domain_id", "logName" => "process_ep_files_php"));
+
+    $query_args = array(
+        "domainid" => $domain_id
+    );
+    $query = "SELECT * FROM scrapped_file_configuration WHERE domain_id = :domainid AND active IS TRUE";
+    $inner_queryData = pdoExecuteQuery($pdo_mysql, $query, $query_args, "query_3");
+
+    $str_pos_productName_start = strpos($file_content, $inner_queryData[0][0]["item_1_start_string"]);
+    $str_pos_productName_end = strpos($file_content, $inner_queryData[0][0]["item_1_start_end"]);
+    $str_pos_productPrice_start = strpos($file_content, $inner_queryData[0][0]["item_2_start_string"]);
+    $str_pos_productPrice_end = strpos($file_content, $inner_queryData[0][0]["item_2_start_end"]);
+
+    $product_name = substr($file_content, $str_pos_productName_start, $str_pos_productName_end);
+    $product_price = substr($file_content, $str_pos_productPrice_start, $str_pos_productPrice_end);
+
+    if ($product_name != false && $product_price != false) {
+        $product_price = preg_replace("/\s/","",$product_price);
+        $product_price = preg_replace("/\./","",$product_price);
+        $product_price = preg_replace("/,/","",$product_price);
+        $product_price = preg_replace("/\$/","",$product_price);
+
+        verbose(array("outputMode" => $verbose_output_mode, "outputMessage" => "Product name: '$product_name'", "logName" => "process_ep_files_php"));
+        verbose(array("outputMode" => $verbose_output_mode, "outputMessage" => "Product price: '$product_price'", "logName" => "process_ep_files_php"));
+
+        verbose(array("outputMode" => $verbose_output_mode, "outputMessage" => "Checking whether the product already exists in DB...", "logName" => "process_ep_files_php"));
+        $query_args = array(
+            "productname" => $product_name
+            ,"idalmacen" => $domain_id
+        );
+        $query = "SELECT id FROM producto WHERE nombre_producto = :productname AND id_almacen = :idalmacen";
+        $inner_queryData = pdoExecuteQuery($pdo_mysql, $query, $query_args, "query_3");
+        if ($inner_queryData[1] == 0) {
+            verbose(array("outputMode" => $verbose_output_mode, "outputMessage" => "Done, the product doesn't exist", "logName" => "process_ep_files_php"));
+
+            $query_args = array(
+                "idalmacen" => $domain_id
+                ,"productname" => $product_name
+                ,"productprice" => $product_price
+            );
+            $query = "INSERT INTO producto (id_almacen, nombre_producto, precio_producto, id_imagen, activo) VALUES (:idalmacen, :productname, :productprice, NULL, 1)";
+            #pdoExecuteQuery($pdo_mysql, $query, $query_args, "query_4");
+
+        } else {
+            verbose(array("outputMode" => $verbose_output_mode, "outputMessage" => "Done, the product already exists", "logName" => "process_ep_files_php"));
+            if ($inner_queryData[0][0]["precio_producto"] != $product_price) {
+                verbose(array("outputMode" => $verbose_output_mode, "outputMessage" => "The product price has been updated, from '$product_price' to '".$inner_queryData[0][0]["precio_producto"]."', updating in DB...", "logName" => "process_ep_files_php"));
+
+                $query_args = array(
+                    "productid" => $inner_queryData[0][0]["id"]
+                    ,"productprice" => $product_price
+                );
+                $query = "UPDATE producto SET precio_producto = :productprice WHERE id = :productid";
+                #pdoExecuteQuery($pdo_mysql, $query, $query_args, "query_5");
+            } else {
+                verbose(array("outputMode" => $verbose_output_mode, "outputMessage" => "No product price update found, skipping", "logName" => "process_ep_files_php"));
+            }
+        }
+    } else {
+        if ($product_name != false) {
+            verbose(array("outputMode" => $verbose_output_mode, "outputMessage" => "Product name found: '$product_name', however, no product price found", "logName" => "process_ep_files_php"));
+        }
+        if ($product_price != false) {
+            verbose(array("outputMode" => $verbose_output_mode, "outputMessage" => "Product price found: '$product_price', however, no product name found", "logName" => "process_ep_files_php"));
+        }
+        if ($product_name == false && $product_price == false) {
+            verbose(array("outputMode" => $verbose_output_mode, "outputMessage" => "Neither product name nor price were found", "logName" => "process_ep_files_php"));
+        }
+    }
+
+    verbose(array("outputMode" => $verbose_output_mode, "outputMessage" => "Deleting file...", "logName" => "process_ep_files_php"));
+    $query_args = array(
+        "fileid" => $queryData[0][$x]["id"]
+    );
+    $query = "UPDATE epfiles_queue SET has_been_proccesed = 1 WHERE id = :fileid";
+    pdoExecuteQuery($pdo_sqlite_db, $query, $query_args, "query_6");
+    #unlink($ep_file_path.$epfile_folder."/".$folderName."/".$filename);
+    verbose(array("outputMode" => $verbose_output_mode, "outputMessage" => "Done", "logName" => "process_ep_files_php"));
+}
+
+verbose(array("outputMode" => $verbose_output_mode, "outputMessage" => "Removing processed files from DB...", "logName" => "process_ep_files_php"));
+$query_args = array();
+$query = "DELETE FROM epfiles_queue WHERE has_been_proccesed = 1";
+doExecuteQuery($pdo_sqlite_db, $query, $query_args, "query_7");
+verbose(array("outputMode" => $verbose_output_mode, "outputMessage" => "Done", "logName" => "process_ep_files_php"));
+
+verbose(array("outputMode" => $verbose_output_mode, "outputMessage" => "Script done, exiting", "logName" => "process_ep_files_php"));
 
 ?>
--- queue_ep_files.php
+++ queue_ep_files.php
@@ -10,12 +10,12 @@ include_once "functions.php";
 
 $verbose_output_mode = 0;
 
-verbose(array("outputMode" => 0, "outputMessage" => "Starting the ep_file process script", "logName" => "main_php"));
+verbose(array("outputMode" => 0, "outputMessage" => "Starting the ep_file queue script", "logName" => "queue_ep_files_php"));
 
-verbose(array("outputMode" => 0, "outputMessage" => "Connecting to the CompraFacil database...", "logName" => "main_php"));
+verbose(array("outputMode" => 0, "outputMessage" => "Connecting to the CompraFacil database...", "logName" => "queue_ep_files_php"));
 //Create the PDO connection objects
 $pdo_sqlite_db = pdoCreateConnection(array('db_type' => "sqlite", 'db_host' => realpath(__DIR__).'/misc_database.sqlite3', 'db_user' => "root", 'db_pass' => "", 'db_name' => ""));
-verbose(array("outputMode" => 0, "outputMessage" => "Done", "logName" => "main_php"));
+verbose(array("outputMode" => 0, "outputMessage" => "Done", "logName" => "queue_ep_files_php"));
 
 //Define the constants
 $current_script_path = realpath(__DIR__)."/";
@@ -23,18 +23,18 @@ $epfile_folder = "ep_files";
 $ep_file_path = "/mnt/nfs/";
 $ignored_files = array('.', '..');
 
-verbose(array("outputMode" => $verbose_output_mode, "outputMessage" => "Getting the directory listing from path '".$ep_file_path.$epfile_folder."'", "logName" => "main_php"));
+verbose(array("outputMode" => $verbose_output_mode, "outputMessage" => "Getting the directory listing from path '".$ep_file_path.$epfile_folder."'", "logName" => "queue_ep_files_php"));
 $main_directory_listing = scandir($ep_file_path.$epfile_folder,SCANDIR_SORT_ASCENDING);
 
 //Go through the folders and files
 for ($x = 0; $x < sizeof($main_directory_listing); $x++) {
     if (in_array($main_directory_listing[$x], $ignored_files)) {
-        verbose(array("outputMode" => $verbose_output_mode, "outputMessage" => "'".$main_directory_listing[$x]."' is not an accepted file or directory, skipping", "logName" => "main_php"));
+        verbose(array("outputMode" => $verbose_output_mode, "outputMessage" => "'".$main_directory_listing[$x]."' is not an accepted file or directory, skipping", "logName" => "queue_ep_files_php"));
         continue;
     }
     $folder_nydate = $main_directory_listing[$x];
 
-    verbose(array("outputMode" => $verbose_output_mode, "outputMessage" => "Checking if folder ".$folder_nydate." is empty...", "logName" => "main_php"));
+    verbose(array("outputMode" => $verbose_output_mode, "outputMessage" => "Checking if folder ".$folder_nydate." is empty...", "logName" => "queue_ep_files_php"));
     $temp_directory_counter = 0;
     $is_folder_empty = true;
     $temp_file_listing = scandir($ep_file_path.$epfile_folder."/".$folder_nydate,SCANDIR_SORT_ASCENDING);
@@ -55,20 +55,20 @@ for ($x = 0; $x < sizeof($main_directory_listing); $x++) {
     }
 
     if ($is_folder_empty) {
-        verbose(array("outputMode" => $verbose_output_mode, "outputMessage" => "Directory '$folder_nydate' is empty, skipping", "logName" => "main_php"));
+        verbose(array("outputMode" => $verbose_output_mode, "outputMessage" => "Directory '$folder_nydate' is empty, skipping", "logName" => "queue_ep_files_php"));
         continue;
     } else {
-        verbose(array("outputMode" => $verbose_output_mode, "outputMessage" => "Directory '$folder_nydate' is not empty", "logName" => "main_php"));
+        verbose(array("outputMode" => $verbose_output_mode, "outputMessage" => "Directory '$folder_nydate' is not empty", "logName" => "queue_ep_files_php"));
 
         $temp_file_listing = scandir($ep_file_path.$epfile_folder."/".$folder_nydate,SCANDIR_SORT_ASCENDING);
         for ($z = 0; $z < sizeof($temp_file_listing); $z++) {
             $temp_file = $temp_file_listing[$z];
 
             if (in_array($temp_file, $ignored_files)) {
-                verbose(array("outputMode" => $verbose_output_mode, "outputMessage" => "$temp_file' is not an accepted file or directory, skipping", "logName" => "main_php"));
+                verbose(array("outputMode" => $verbose_output_mode, "outputMessage" => "$temp_file' is not an accepted file or directory, skipping", "logName" => "queue_ep_files_php"));
                 continue;
             }
-            verbose(array("outputMode" => $verbose_output_mode, "outputMessage" => "Checking whether file '$temp_file' is already queued...", "logName" => "main_php"));
+            verbose(array("outputMode" => $verbose_output_mode, "outputMessage" => "Checking whether file '$temp_file' is already queued...", "logName" => "queue_ep_files_php"));
 
             $query_args = array(
                 'filename' => $temp_file
@@ -77,20 +77,22 @@ for ($x = 0; $x < sizeof($main_directory_listing); $x++) {
             $queryData = pdoExecuteQuery($pdo_sqlite_db, $query, $query_args, "query_1");
 
             if ($queryData[1] == 0) {
-                verbose(array("outputMode" => $verbose_output_mode, "outputMessage" => "The file has not been queued, queuing", "logName" => "main_php"));
+                verbose(array("outputMode" => $verbose_output_mode, "outputMessage" => "The file has not been queued, queuing", "logName" => "queue_ep_files_php"));
                 $query_args = array(
                     'filename' => $temp_file,
                     'datefolder' => $folder_nydate
                 );
                 $query = "INSERT INTO epfiles_queue (ep_file_name, has_been_proccesed, date_folder, is_in_process) VALUES (:filename, 0, :datefolder, 0)";
                 pdoExecuteQuery($pdo_sqlite_db, $query, $query_args, "query_2");
-                verbose(array("outputMode" => $verbose_output_mode, "outputMessage" => "Done", "logName" => "main_php"));
+                verbose(array("outputMode" => $verbose_output_mode, "outputMessage" => "Done", "logName" => "queue_ep_files_php"));
             } else {
-                verbose(array("outputMode" => $verbose_output_mode, "outputMessage" => "The file has already been queued, skipping", "logName" => "main_php"));
+                verbose(array("outputMode" => $verbose_output_mode, "outputMessage" => "The file has already been queued, skipping", "logName" => "queue_ep_files_php"));
                 continue;
             }
         }
     }
 }
+
+verbose(array("outputMode" => $verbose_output_mode, "outputMessage" => "Script done, exiting", "logName" => "process_ep_files_php"));
 
 ?>
